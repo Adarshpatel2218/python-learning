921908919732cd11369b683f94b151e48c3341cb
Adarsh Patel <adarshpatel@Adarshs-MacBook-Air-5.local>
2026-06-26 11:29:47 +0530

    update

--- face_service.py
+++ face_service.py
@@ -2,10 +2,9 @@ import cv2
 import numpy as np
 from insightface.app import FaceAnalysis
 
-# Model load only once
 face_app = FaceAnalysis(
     name="buffalo_l",
-    providers=["CPUExecutionProvider"]   # GPU ho to CUDAExecutionProvider
+    providers=["CPUExecutionProvider"] 
 )
 
 face_app.prepare(
@@ -15,17 +14,12 @@ face_app.prepare(
 
 
 def get_embedding(image_bytes):
-
-    # Convert bytes -> numpy
     np_arr = np.frombuffer(image_bytes, np.uint8)
-
-    # Decode image
     img = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
 
     if img is None:
         return None
 
-    # Large image resize
     h, w = img.shape[:2]
 
     if max(h, w) > 1280:
@@ -41,7 +35,6 @@ def get_embedding(image_bytes):
     if not faces:
         return None
 
-    # Highest confidence face
     face = max(faces, key=lambda x: x.det_score)
 
     return face.embedding.astype(np.float32).tolist()
